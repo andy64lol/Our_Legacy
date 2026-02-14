b2b3c39782db016df07a6aa0edea4877a26ac1fd
andy64lol <andy64lolxd@gmail.com>
2026-02-14 14:44:52 +0100

    refactored them to handle CORS

--- functions/ping.js
+++ functions/ping.js
@@ -1,9 +1,26 @@
 // api/ping.js
 
 exports.handler = async function(event, context) {
+  // CORS headers
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Content-Type': 'application/json'
+  };
+  
+  // Handle OPTIONS request for CORS preflight
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: headers,
+      body: ''
+    };
+  }
+  
   return {
     statusCode: 200,
-    headers: { "Content-Type": "application/json" },
+    headers: headers,
     body: JSON.stringify({
       ok: true,
       message: "Ping successful! Serverless is alive and responding.",
